d8f60474de3fc6b4ac2cce9dc01ca2fc3c0c3f22
ebarkett <elizabeth.barkett@udri.udayton.edu>
2024-08-12 14:39:19 -0400

    add check in successful syncs file before pushing blobs

--- internal/actions/mirror/batchDeserialize.go
+++ internal/actions/mirror/batchDeserialize.go
@@ -25,7 +25,16 @@ func (action *BatchDeserialize) Run(ctx context.Context, syncDir, destination st
 	log := logger.FromContext(ctx)
 
 	var successfulSyncs [][]string
-
+	file, err := os.OpenFile(filepath.Join(syncDir, action.SuccessfulSyncFile), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return fmt.Errorf("opening successful sync file: %w", err)
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	existingSyncs, err := r.ReadAll()
+	if err != nil {
+		return fmt.Errorf("reading successful sync file: %w", err)
+	}
 	// all of the tar files live in syncDir/data directory.
 	// get all of the files within syncDir/data, ignore the trackerfile.
 	if err := filepath.Walk(syncDir, func(path string, info fs.FileInfo, err error) error {
@@ -39,13 +48,18 @@ func (action *BatchDeserialize) Run(ctx context.Context, syncDir, destination st
 		if info.IsDir() {
 			return nil
 		}
+		if isSynced(info.Name(), existingSyncs) {
+			log.InfoContext(ctx, "File has previously been synced. Skipping...", "filename", info.Name())
+			// file has previously been synced so we don't want to push those blobs again.
+			return nil
+		}
 		// parse the file to get the image tag.
 		splitName := strings.Split(info.Name(), "-")
 		if len(splitName) != 2 {
 			return fmt.Errorf("unexpected file name %s. Batch Deserialize expects tar files with an int-name format, e.g., 0-image1, 1-image2, etc", info.Name())
 		}
 		// create the destination target
-		repo, err := action.Config.Repository(ctx, strings.Join([]string{destination, splitName[1]}, ":"))
+		repo, err := action.Config.Repository(ctx, strings.Join([]string{destination, strings.Split(splitName[1], ".")[0]}, ":"))
 		if err != nil {
 			return err
 		}
@@ -71,14 +85,18 @@ func (action *BatchDeserialize) Run(ctx context.Context, syncDir, destination st
 		return err
 	}
 	// write out to successful_syncs.txt the tar file name and the date/timestamp.
-	file, err := os.OpenFile(filepath.Join(syncDir, action.SuccessfulSyncFile), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		return fmt.Errorf("opening successful sync file: %w", err)
-	}
-	defer file.Close()
 	w := csv.NewWriter(file)
 	if err := w.WriteAll(successfulSyncs); err != nil {
 		return fmt.Errorf("writing to successful syncs file: %w", err)
 	}
 	return nil
+}
+
+func isSynced(filename string, existingSyncs [][]string) bool {
+	for _, line := range existingSyncs {
+		if filename == line[0] {
+			return true
+		}
+	}
+	return false
 }
