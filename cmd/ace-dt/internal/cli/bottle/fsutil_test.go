f22f6a34c2814e84087b5176b99a2b6268189dad
Elizabeth Barkett <elizabeth.barkett@udri.udayton.edu>
2025-02-04 14:09:37 +0000

    fix(test): fsutil test

--- cmd/ace-dt/internal/cli/bottle/fsutil_test.go
+++ cmd/ace-dt/internal/cli/bottle/fsutil_test.go
@@ -94,6 +94,8 @@ func TestAddFileWithData(t *testing.T) {
 }
 
 func TestAddFileOfSize(t *testing.T) {
+	absoluteFilepath, err := filepath.Abs("/invalid/data.txt")
+	require.NoError(t, err, "filepath.Abs should not return an error")
 	testCases := []struct {
 		name   string
 		path   string
@@ -117,9 +119,9 @@ func TestAddFileOfSize(t *testing.T) {
 		},
 		{
 			name:   "Invalid absolute path",
-			path:   "//invalid/random.txt",
+			path:   absoluteFilepath,
 			size:   1024,
-			errMsg: "path //invalid/random.txt is absolute. All FSUtil paths are relative",
+			errMsg: fmt.Sprintf("path %s is absolute. All FSUtil paths are relative", absoluteFilepath),
 		},
 	}
 
@@ -147,6 +149,8 @@ func TestAddFileOfSize(t *testing.T) {
 }
 
 func TestAddDir(t *testing.T) {
+	absoluteFilepath, err := filepath.Abs("/invalid/data.txt")
+	require.NoError(t, err, "filepath.Abs should not return an error")
 	testCases := []struct {
 		name   string
 		path   string
@@ -166,8 +170,8 @@ func TestAddDir(t *testing.T) {
 		},
 		{
 			name:   "Invalid absolute path",
-			path:   "//invalid/random",
-			errMsg: "path //invalid/random is absolute. All FSUtil paths are relative",
+			path:   absoluteFilepath,
+			errMsg: fmt.Sprintf("path %s is absolute. All FSUtil paths are relative", absoluteFilepath),
 		},
 	}
 
@@ -195,6 +199,8 @@ func TestAddDir(t *testing.T) {
 }
 
 func TestAddFileOfSizeDeterministic(t *testing.T) {
+	invalidDeterministic, err := filepath.Abs("/invalid/data.txt")
+	require.NoError(t, err, "filepath.Abs should not return an error")
 	testCases := []struct {
 		name   string
 		path   string
@@ -218,9 +224,9 @@ func TestAddFileOfSizeDeterministic(t *testing.T) {
 		},
 		{
 			name:   "Invalid absolute path",
-			path:   "//invalid/deterministic.txt",
+			path:   invalidDeterministic,
 			size:   1024,
-			errMsg: "path //invalid/deterministic.txt is absolute. All FSUtil paths are relative",
+			errMsg: fmt.Sprintf("path %s is absolute. All FSUtil paths are relative", invalidDeterministic),
 		},
 	}
 
