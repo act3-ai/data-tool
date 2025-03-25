04682c418623a28176af3fe42cad8981b965d04b
Nathan Joslin <nathan.joslin@udri.udayton.edu>
2025-03-25 15:43:20 -0400

    fix(scan): handling of grype v0.90.0 checksum parsing

--- internal/security/command.go
+++ internal/security/command.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	"regexp"
 	"strings"
 
 	"git.act3-ace.com/ace/data/tool/internal/ui"
@@ -85,13 +84,69 @@ func grypeSBOM(ctx context.Context, sbom io.ReadCloser) (*Results, error) {
 }
 
 func getGrypeDBChecksum(ctx context.Context) (string, error) {
-	cmd := exec.CommandContext(ctx, "grype", "db", "status")
+	cmd := exec.CommandContext(ctx, "grype", "db", "status", "--output", "json")
 	res, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("getting the grype db checksum: %w", err)
 	}
-	pattern := `Checksum:\s*([a-z0-9\:]+)`
-	re := regexp.MustCompile(pattern)
-	checksum := re.FindSubmatch(res)
-	return string(checksum[1]), nil
+
+	var grypeStatus grypeDBChecksum
+	if err := json.Unmarshal(res, &grypeStatus); err != nil {
+		return "", fmt.Errorf("decoding grype db checksum output: %w", err)
+	}
+
+	return grypeStatus.value()
+}
+
+// The following is for documenting changes to the output of grype db status
+// due to frequent changes.
+// v0.87.0 and prior:
+//
+//	struct {
+//		SchemaVersion int
+//		Built         string // time
+//		Location      string // filepath
+//		Checksum      string // sha256 hash
+//		Error     	  struct
+//	}
+//
+// v0.88.0 & v0.89.0:
+//
+//	struct {
+//		SchemaVersion string // type is now string
+//		Built         string
+//		Path      	  string // Location renamed to Path
+//		Checksum      string // change in format, likely a non-cryptographic digest
+//		Error     	  string // error no longer a struct
+//	}
+//
+// v0.90.0 (latest at this time):
+//
+//	struct {
+//		SchemaVersion string // v6.0.2
+//		Built         string
+//		Path      	  string
+//		From          string // Checksum renamed to From, another format change to a URL reference to a tar.zst file (with checksum)
+//		Valid     	  bool   // Error renamed to Valid, type is now boolean
+//	}
+
+// grypeDBChecksum contains all possible keys for the "checksum" field as output
+// by 'grype db status'.
+type grypeDBChecksum struct {
+	Checksum string `json:"checksum,omitempty"` // v0.89.0 and prior
+	From     string `json:"from,omitempty"`     // v0.90.0 (latest at this time)
+}
+
+func (g *grypeDBChecksum) value() (string, error) {
+	switch {
+	case g.From != "":
+		// grype v0.90.0 and later
+		return g.From, nil
+	case g.Checksum != "":
+		// grype v0.89.0 and prior
+		return g.Checksum, nil
+	default:
+		// theoretically impossible as 'grype db status' should throw an error first
+		return "", fmt.Errorf("both checksum and from fields are empty, please run 'grype db status' to validate")
+	}
 }
